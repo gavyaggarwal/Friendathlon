c9dd373cca01324a0a5df6e74627ee24e8e4af93
Gavy Aggarwal <gavyaggarwal@gmail.com>
2016-09-13 17:37:40 -0400

    Open shift integration

---
+++ server/index.js
@@ -0,0 +1,22 @@
+//  OpenShift sample Node application
+var express = require('express'),
+    app     = express()
+
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+// error handling
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500).send('Something bad happened!');
+});
+
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+module.exports = app ;
