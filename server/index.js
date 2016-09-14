61d3a87957e29a0aa20591a2d794aff708302ff5
Gavy Aggarwal <gavyaggarwal@gmail.com>
2016-09-13 22:32:27 -0400

    Fixed server to work offline

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL || 'mongodb://localhost:27017/friendathlon',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
@@ -33,7 +33,10 @@ if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
     mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
 
   }
+} else {
+  mongoURL = "mongodb://localhost:27017/friendathlon";
 }
+
 var db = null,
     dbDetails = new Object();
 
