a5e73aa2bdea0736818c8fed5fdf7b4b010c13e6
Gavy Aggarwal <gavyaggarwal@gmail.com>
2016-09-13 20:56:30 -0400

    Server tweak

--- server/index.js
+++ server/index.js
@@ -69,10 +69,10 @@ app.get('/', function (req, res) {
     // Create a document with request IP and current time of request
     col.insert({ip: req.ip, date: Date.now()});
     col.count(function(err, count){
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+      res.send(JSON.stringify({ pageCountMessage : count, dbInfo: dbDetails }));
     });
   } else {
-    res.render('index.html', { pageCountMessage : null});
+    res.send("NO COUNT");
   }
 });
 
