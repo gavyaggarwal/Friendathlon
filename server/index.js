a0a3c06e9c4cb3e0cd6437e46732b5a0fafc1b96
Gavy Aggarwal <gavyaggarwal@gmail.com>
2016-09-13 22:08:54 -0400

    Setup test api

--- server/index.js
+++ server/index.js
@@ -58,37 +58,45 @@ var initDb = function(callback) {
   });
 };
 
-app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
+function runIfDB(func, res) {
   if (!db) {
     initDb(function(err){});
   }
   if (db) {
+    func();
+  } else {
+    res.send(JSON.stringify({"success": false}));
+  }
+}
+
+function sendObject(res, obj) {
+  obj["success"] = true;
+  res.send(JSON.stringify(obj));
+}
+
+app.get('/test', function (req, res) {
+  runIfDB(function() {
+    sendObject({"appName":"Friendathlon"});
+  }, res);
+});
+
+app.get('/', function (req, res) {
+  runIfDB(function() {
     var col = db.collection('counts');
     // Create a document with request IP and current time of request
     col.insert({ip: req.ip, date: Date.now()});
     col.count(function(err, count){
       res.send(JSON.stringify({ pageCountMessage : count, dbInfo: dbDetails }));
     });
-  } else {
-    res.send("NO COUNT");
-  }
+  }, res);
 });
 
 app.get('/pagecount', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
+  runIfDB(function() {
     db.collection('counts').count(function(err, count ){
       res.send('{ pageCount: ' + count + '}');
     });
-  } else {
-    res.send('{ pageCount: -1 }');
-  }
+  }, res);
 });
 
 // error handling
