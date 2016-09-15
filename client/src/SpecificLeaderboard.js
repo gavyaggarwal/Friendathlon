8e4e91c1cda0e9d740e9533eb0b7551f0e600493
Gavy Aggarwal <gavyaggarwal@gmail.com>
2016-09-14 22:11:37 -0400

    updated login button

--- client/src/SpecificLeaderboard.js
+++ client/src/SpecificLeaderboard.js
@@ -1,3 +1,7 @@
+/*
+TODO: Update friends list on server each time app is started
+*/
+
 import React, { Component } from 'react';
 import {
   Text,
@@ -13,17 +17,46 @@ const {
 
 import Styles from './Styles';
 
+class Table extends Component {
+  render() {
+    if (this.props.items == undefined || this.props.items == []) {
+      return (
+        <Text>No items</Text>
+      )
+    } else {
+      return (
+        <Text>Items</Text>
+      )
+    }
+  }
+}
+
 export default class SpecificLeaderboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  loggedIn(error, result) {
+    console.log(error, result);
+    alert("logged in");
+  }
+  loggedOut() {
+    alert('logged out');
+  }
   render() {
     return (
       <View style={Styles.container}>
-        <LoginButton />
+        <LoginButton
+          publishPermissions={["user_friends"]}
+          onLoginFinished={this.loggedIn}
+          onLogoutFinished={this.loggedOut} />
         <Text style={Styles.welcome}>
           Specific Leaderboard will go here
         </Text>
         <TouchableHighlight onPress={this.props.navigator.pop}>
           <Text style={Styles.button}>Back</Text>
         </TouchableHighlight>
+        <Table items={this.state.friends} />
       </View>
     );
   }
