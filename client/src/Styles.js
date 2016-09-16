834734410604ff0bd5fca473392c10f49046c099
abiramikv <abi1kur11ven22@gmail.com>
2016-09-16 09:22:08 -0700

    setup fonts and signup page

--- client/src/Styles.js
+++ client/src/Styles.js
@@ -11,21 +11,30 @@ var exports = module.exports = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f5f3e9',
+    backgroundColor: '#F5F3E9',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 25,
     textAlign: 'center',
     margin: 10,
+    fontWeight: 'bold',
+    fontFamily: 'alegreyasans',
   },
   button: {
     backgroundColor: 'red',
     color: 'white',
-    padding: 10
+    padding: 10,
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+    fontSize: 20,
+    fontFamily: 'alegreyasans_light',
+  },
+  connectBtn: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
 });
